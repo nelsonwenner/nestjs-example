28a5d8f530bbd891739c1f21d6a676632cfe1748
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-08 17:42:44 -0300

    feature: add auth on endpoints

--- nestjs-crud-idea/src/core/users/users.controller.ts
+++ nestjs-crud-idea/src/core/users/users.controller.ts
@@ -1,13 +1,15 @@
 import { UserDTO } from './users.dto';
 import { UsersService } from './users.service';
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../shared/auth.guard';
 
 @Controller('api/v1/users')
 export class UsersController {
 
     constructor(private usersService: UsersService) {}
-
+    
     @Get()
+    @UseGuards(new AuthGuard())
     showAll() {
         return this.usersService.showAll();
     }
